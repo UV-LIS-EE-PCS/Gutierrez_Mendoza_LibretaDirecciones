cacbbaa9a1392b13a5439ea8beea54758f053d60
julio <zS2201737@estudiantes.uv.mx>
2024-05-25 21:39:59 -0600

    construcción de AddressEntry

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +11,25 @@ public class AddressBook {
 
     public void addAddress(AddressEntry entry) {
         entries.add(entry);
+    }
+
+    public void removeAddress(AddressEntry entry) {
+        entries.remove(entry);
+    }
+
+    public List<AddressEntry> findEntriesByLastName(String lastName) {
+        List<AddressEntry> results = new ArrayList<>();
+        for (AddressEntry entry : entries) {
+            if (entry.getLastName().equalsIgnoreCase(lastName)) {
+                results.add(entry);
+            }
+        }
+        return results;
+    }
+
+    public void printAllEntries() {
+        for (AddressEntry entry : entries) {
+            System.out.println(entry);
+        }
     }
 }
---
+++ src/main/java/AddressEntry.java
@@ -0,0 +1,104 @@
+public class AddressEntry {
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String email;
+    private String phone;
+
+    // Constructor
+    public AddressEntry(String firstName, String lastName, String street, String city,
+                        String state, String zip, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "AddressEntry{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
