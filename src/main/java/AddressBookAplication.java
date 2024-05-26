1dc32c934a849d4a2957bf74c2997b1403f8eb0e
julio <zS2201737@estudiantes.uv.mx>
2024-05-26 16:54:47 -0600

    Aplication

--- src/main/java/AddressBookAplication.java
+++ src/main/java/AddressBookAplication.java
@@ -3,6 +3,17 @@ public class AddressBookAplication {
     private Menu menu;
 
 
+    public class AddressBookApplication {
+        public static void main(String[] args) {
+            Menu menu = new Menu();
+            Scanner in = new Scanner(System.in);
+            AddressBook addressBook = AddressBook.getInstance();
 
+            do{
+
+
+        }while (true);
+
+    }
 }
 
