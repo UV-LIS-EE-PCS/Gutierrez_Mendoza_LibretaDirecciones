7398d60665e62153f61e4b766681066709cfcf91
julio <zS2201737@estudiantes.uv.mx>
2024-05-25 21:43:39 -0600

    construcción de Menu

---
+++ src/main/java/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public void displayMenu() {
+        System.out.println("====================================");
+        System.out.println("Hola! Elige una opción del menú:");
+        System.out.println("a) Cargar del archivo");
+        System.out.println("b) Agregar");
+        System.out.println("c) Eliminar");
+        System.out.println("d) Buscar");
+        System.out.println("e) Mostrar");
+        System.out.println("f) Salir");
+        System.out.println("====================================");
+    }
+}
