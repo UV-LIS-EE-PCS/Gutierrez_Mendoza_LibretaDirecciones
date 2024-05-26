31736edfa92724f78b1027b24430e09a3296fe9e
julio <zS2201737@estudiantes.uv.mx>
2024-05-25 21:32:19 -0600

    AddressEntry

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -11,24 +11,4 @@ public class AddressBook {
     public void addAddress(AddressEntry entry) {
         entries.add(entry);
     }
-
-    public void removeAddress(AddressEntry entry) {
-        entries.remove(entry);
-    }
-
-    public List<AddressEntry> findEntriesByLastName(String lastName) {
-        List<AddressEntry> results = new ArrayList<>();
-        for (AddressEntry entry : entries) {
-            if (entry.getLastName().equalsIgnoreCase(lastName)) {
-                results.add(entry);
-            }
-        }
-        return results;
-    }
-
-    public void printAllEntries() {
-        for (AddressEntry entry : entries) {
-            System.out.println(entry);
-        }
-    }
 }
