c8365a517c0dc4116d97ab9c8b6f2b0b758c5f06
julio <zS2201737@estudiantes.uv.mx>
2024-05-26 17:11:41 -0600

    AddressEntryCorreccion

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,35 +1,94 @@
 
+import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
 public class AddressBook {
-    private List<AddressEntry> entries;
+    private static AddressBook instance = null;
+    private ArrayList<AddressEntry> entriesList;
 
-    public AddressBook() {
-        entries = new ArrayList<>();
+    private AddressBook() {
+        this.entriesList = new ArrayList<>();
     }
 
-    public void addAddress(AddressEntry entry) {
-        entries.add(entry);
+    public static AddressBook getInstance() {
+        if (instance == null) {
+            instance = new AddressBook();
+        }
+        return instance;
     }
 
-    public void removeAddress(AddressEntry entry) {
-        entries.remove(entry);
-    }
+    public void showContactsList() {
+        // TODO: Order by last name
+        if (entriesList.isEmpty()) System.out.println("La lista esta vacia:(");
+        else {
+            // Definir el comparador
+            Comparator<AddressEntry> byLastName = Comparator.comparing(AddressEntry::getLastName);
 
-    public List<AddressEntry> findEntriesByLastName(String lastName) {
-        List<AddressEntry> results = new ArrayList<>();
-        for (AddressEntry entry : entries) {
-            if (entry.getLastName().equalsIgnoreCase(lastName)) {
-                results.add(entry);
+            // Ordenar la lista
+            entriesList.sort(byLastName);
+            for (AddressEntry entry : entriesList) {
+                System.out.println((entriesList.indexOf(entry) + 1) + ". " + entry.toString() + "\n");
             }
         }
-        return results;
     }
 
-    public void printAllEntries() {
-        for (AddressEntry entry : entries) {
-            System.out.println(entry);
+    public ArrayList<AddressEntry> findAddressEntry(String lastName, boolean strict) throws Exception {
+        if (strict) lastNameExistInContactsList(lastName);
+        ArrayList<AddressEntry> matchingContacts = new ArrayList<>();
+        if (entriesList.isEmpty()) {
+            throw new Exception("La lista esta vacia:(");
+        } else {
+            for (AddressEntry entry : entriesList) {
+                if (entry.getLastName().toLowerCase().contains(lastName.toLowerCase())) {
+                    matchingContacts.add(entry);
+                }
+            }
+        }
+        return matchingContacts;
+    }
+
+    public void lastNameExistInContactsList(String lastName) throws Exception {
+        if (entriesList.stream().noneMatch(entry -> entry.getLastName().equalsIgnoreCase(lastName))) {
+            throw new Exception("El apellido no existe en la lista de contactos");
+        }
+    }
+
+    public void addAddressEntry(AddressEntry addressEntry) {
+        entriesList.add(addressEntry);
+    }
+
+    public void deleteAddressEntry(String lastName) {
+        entriesList.removeIf(entry -> entry.getLastName().equalsIgnoreCase(lastName));
+    }
+
+    public void readFromATextFile(String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String[] contactData = new String[8];
+            int index = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\n");
+                contactData[index] = parts[0];
+                index++;
+            }
+            AddressEntry entry = new AddressEntry(contactData[0], contactData[1], contactData[2], contactData[3], contactData[4], Integer.parseInt(contactData[5]), contactData[6], contactData[7]);
+            addAddressEntry(entry);
+            System.out.println("Entries loaded from file.");
+        } catch (IOException e) {
+            System.err.println("Error loading from file: " + e.getMessage());
+        }
+    }
+
+    public void showContactsFound(ArrayList<AddressEntry> contacts) {
+        if (contacts.size() == 1) {
+            System.out.println("El siguiente contacto fue encontrado:");
+            System.out.println(contacts.toString());
+        } else {
+            System.out.println("Los siguientes " + contacts.size() + " contactos fueron encontrados");
+            for (AddressEntry entry: contacts) {
+                System.out.println((entriesList.indexOf(entry) + 1) + ". " + entry.toString());
+            }
         }
     }
 }
--- src/main/java/AddressEntry.java
+++ src/main/java/AddressEntry.java
@@ -1,104 +1,96 @@
 public class AddressEntry {
-    private String firstName;
-    private String lastName;
-    private String street;
-    private String city;
-    private String state;
-    private String zip;
-    private String email;
-    private String phone;
+    private String name, lastName, street, city, state;
+    private int postalCode;
+    private String email, phone;
 
-    // Constructor
-    public AddressEntry(String firstName, String lastName, String street, String city,
-                        String state, String zip, String email, String phone) {
-        this.firstName = firstName;
+    public AddressEntry () {
+
+    }
+
+    public AddressEntry(String name, String lastName, String street, String city, String state, int postalCode, String email, String phone) {
+        this.name = name;
         this.lastName = lastName;
         this.street = street;
         this.city = city;
         this.state = state;
-        this.zip = zip;
+        this.postalCode = postalCode;
         this.email = email;
         this.phone = phone;
     }
 
-    // Getters
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public String getStreet() {
-        return street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    // Setters
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
     }
 
     public void setStreet(String street) {
         this.street = street;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getState() {
+        return state;
     }
 
     public void setState(String state) {
         this.state = state;
     }
 
-    public void setZip(String zip) {
-        this.zip = zip;
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    // toString method
     @Override
     public String toString() {
-        return "AddressEntry{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", street='" + street + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip='" + zip + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
+        return "Nombre: " + name + '\n' +
+                "Apellido: " + lastName + '\n' +
+                "Calle: " + street + '\n' +
+                "Ciudad: " + city + '\n' +
+                "Estado: " + state + '\n' +
+                "Código postal: " + postalCode  +'\n' +
+                "Email: " + email + '\n' +
+                "Teléfono: " + phone;
     }
 }
